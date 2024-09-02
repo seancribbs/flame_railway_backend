608ab50c34601e0e7944ae69675802a6ba34bb1c
Sean Cribbs <seancribbs@gmail.com>
2024-09-02 17:11:57 -0500

    Disable debug logs on the gql client

--- lib/flame/railway_backend/neuron_connection.ex
+++ lib/flame/railway_backend/neuron_connection.ex
@@ -39,7 +39,7 @@ defmodule FLAME.RailwayBackend.NeuronConnection do
     plug Tesla.Middleware.BearerAuth, token: System.fetch_env!("RAILWAY_TOKEN")
     plug Tesla.Middleware.Headers, [{"content-type", "application/json"}]
     plug Tesla.Middleware.DecodeJson, engine: FLAME.RailwayBackend.NeuronConnection.Json
-    plug Tesla.Middleware.Logger, debug: true
+    plug Tesla.Middleware.Logger
 
     def opentelemetry_opts do
       [
